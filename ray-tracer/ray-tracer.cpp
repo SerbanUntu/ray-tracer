81aa2b44227076c7b8807edc7c4d341ec5511b48
Serban Untu <133024956+SerbanUntu@users.noreply.github.com>
2025-07-11 10:35:23 +0100

    init: cmake project

---
+++ ray-tracer/ray-tracer.cpp
@@ -0,0 +1,12 @@
+﻿// ray-tracer.cpp : Defines the entry point for the application.
+//
+
+#include "ray-tracer.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
