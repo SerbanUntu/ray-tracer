a79b34f2892aca6788f24bd87892ee8fec0fa5b3
Serban Untu <133024956+SerbanUntu@users.noreply.github.com>
2025-08-16 11:49:59 +0300

    feat: add mock tests

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,2 +1,18 @@
-#include <util/vec3.h>
 #include <gtest/gtest.h>
+
+TEST(Test, TestOnePlusOne) {
+	const auto expected = 2;
+	const auto actual = 1 + 1;
+	ASSERT_EQ(expected, actual);
+}
+
+TEST(Test, TestTwoPlusTwo) {
+	const auto expected = 4;
+	const auto actual = 2 + 2;
+	ASSERT_EQ(expected, actual);
+}
+
+int main(int argc, char** argv) {
+	::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
